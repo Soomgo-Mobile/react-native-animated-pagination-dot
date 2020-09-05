692151bca202198767fb83fd8ac04df3cdb39d5a
pratt <pratt@soomgo.com>
2020-09-05 14:31:32 +0900

    add import

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@
  *
  */
 import React from 'react';
-import {ScrollView, View, Platform, StyleSheet} from "react-native";
+import {ScrollView, View, ViewStyle, StyleProp} from "react-native";
 import Dot from './component/Dot';
 import EmptyDot, {defaultEmptyDotSize} from './component/EmptyDot';
 
