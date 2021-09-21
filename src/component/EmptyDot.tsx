068d8b2d9918b6931395744af8bf31bac7124a04
Pratt Yeon <pratt@soomgo.com>
2021-09-21 15:14:10 +0900

    feat: [:art:] change from return react element return react fc & remove IPropEmptyDot

--- src/component/EmptyDot.tsx
+++ src/component/EmptyDot.tsx
@@ -3,25 +3,32 @@
  * Created by rouge on 11/09/2019.
  */
 import React from 'react';
-import { View } from 'react-native';
-import { IPropEmptyDot } from './types/Dot';
+import { StyleSheet, View } from 'react-native';
 
 export const defaultEmptyDotSize = 3;
 
-const EmptyDot = (
-  props: IPropEmptyDot
-): React.FunctionComponentElement<IPropEmptyDot> => {
+const EmptyDot: React.FC<{
+  sizeRatio: number;
+}> = (props) => {
   return (
     <View
-      style={{
-        backgroundColor: 'white',
-        opacity: 0.0,
-        width: defaultEmptyDotSize * props.sizeRatio,
-        height: defaultEmptyDotSize * props.sizeRatio,
-        margin: defaultEmptyDotSize * props.sizeRatio,
-      }}
+      style={[
+        styles.base,
+        {
+          width: defaultEmptyDotSize * props.sizeRatio,
+          height: defaultEmptyDotSize * props.sizeRatio,
+          margin: defaultEmptyDotSize * props.sizeRatio,
+        },
+      ]}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: 'white',
+    opacity: 0.0,
+  },
+});
 
 export default EmptyDot;
